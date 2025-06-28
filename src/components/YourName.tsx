f6fcdfcef3820228092c780fca9297e3b7dcb439
XavierJoe10 <kjxavierpalachode10.com>
2025-06-28 13:52:18 +0530

    changed name

--- src/components/YourName.tsx
+++ src/components/YourName.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const YourName: React.FC = () => {
   return (
     <span className="bg-gradient-to-r from-primary-600 to-secondary-600 bg-clip-text text-transparent">
-      Alex Johnson
+      Xavier
     </span>
   );
 };
